69f3f1c8e1e3f28f2672aa8137de87b4cb786eee
matt <mattpalms@gmail.com>
2022-12-29 14:18:03 +0000

    Cleaned up versions of hash chain.
      * avoids use of goto statement
         - splits out search_chain into separate function.
         - main code loop is much clearer.
      * add defines for common constants.
      * update documentation.

--- hc2.c
+++ hc2.c
@@ -2,87 +2,143 @@
  * Copyright 2022 Matt Palmer.  All rights reserved.
  *
  * This is an implementation of the HashChain algorithm, (currently unpublished) by Matt Palmer.
+ * It is a factor search similar to WFR or the QF family of algorithms.
  *
- * It is written to integrate with the SMART string search benchmarking tool, by Simone Faro and Thierry Lecroq.
+ * It builds a hash table containing entries for chains of hashes.  Hashes are chained together by
+ * placing a fingerprint of the *next* hash into the entry for the *current* hash.  This enables
+ * a check for the second hash value to be performed without requiring a second lookup in the hash table.
  *
- * It is a factor search similar to WFR or QF family of algorithms.
- * It builds a hash table containing an entry for each of the qgrams and for any prior qgrams in the pattern
- * leading up to it.  Subsequent q-grams are chained to each preceding hash entry by a rolling hash.
- * We put a fingerprint of the next qgram hash value in the preceding (from right to left) qgram hash value.
- * This creates a chain of hash values, where each value has to have a fingerprint of the *next* hash value
- * in the chain.  We can then check whether the next hash probably exists in the chain without incurring another index lookup.
+ * A rolling hash function is used for each iteration of the chain, which has three main effects:
+ *
+ * 1) It expands the effective alphabet of the pattern, which makes it work better on lower alphabet data.
+ * 2) It creates multiple chains of hashes, which increases the load on the hash table.
+ *    Each position in the pattern is an "anchor" hash, which is linked to a chain of q-grams back to the start of the pattern.
+ * 3) Because it is a rolling hash, eventually the hash values converge on the same sequences, so we only have to process
+ *    part of each chain to ensure a complete set of entries all the way back to the start of the pattern in the hash table.
+ *
+ * This implementation is written to integrate with the SMART string search benchmarking tool,
+ * by Simone Faro, Matt Palmer, Stefano Stefano Scafiti and Thierry Lecroq.
  */
 
 #include "include/define.h"
-#include "include/log2.h"
 #include "include/main.h"
+#include "math.h"
 
-#define	Q     2     // qgram length.
-#define S1    3     // bit shift for each of the anchor hash bytes.
-#define S2    4     // rolling hash shift.  smaller values create longer hash chains.
-#define S3    1     // bit shift for each of the chain hash bytes.
-#define ASIZE 2048  // Must be a power of two size, minimum size is 32.
+/*  Tuning algorithm notes.
+ *
+ *  - Shorter patterns often benefit most from larger table sizes, as they rely more on blank hash table entries.
+ *  - Longer patterns often work better with smaller hash table sizes.  They are not primarily relying on blank
+ *    entries for their speed, and can benefit more from having better cache-hits on the hash table.
+ */
 
-#define TABLE_MASK (ASIZE - 1) // mask for table is one less than the power of two size.
+/*
+ * Hash table size.  Must be a power of two, minimum size is 32.
+ */
+#define ASIZE 2048
 
-#define HASH(x, p, s)     ((x[p] << (s)) + x[p - 1])  // General hash function using a bitshift for each byte added.
-#define ANCHOR_HASH(x, p) HASH((x), (p), (S1))        // Hash function for anchor hashes, using the S1 bitshift.
-#define CHAIN_HASH(x, p)  HASH((x), (p), (S3))        // Hash function for chain hashes, using the S3 bitshift.
-#define FINGERPRINT(x)    (1U << ((x) & 0x1F))        // Hash fingerprint, taking low 5 bits of the hash to set one of 32 bits.
+/*
+ * Bit shift for each of the anchor hash byte components.
+ * We want to ensure a reasonably good spread and mixing of initial values over the hash table given Q bytes.
+ */
+#define S1    3
+
+/*
+ * Rolling hash bit-shift.  This shifts the previous hash value over by this number of bits.
+ * Lower values give longer hash chains (more entries in the hash table).
+ * We find that very long chains do not improve performance, but neither do the shortest chains.
+ * Setting to 4 seems to work well.
+ */
+#define S2    4
+
+/*
+ * Bit shift for each of the chain hash byte components.
+ * This is added to the anchor hash, which should already have a fairly good spread of initial values.
+ * We find that very low values of this bit shift work best in general, and higher values usually don't.
+ */
+#define S3    1
+
+/*
+ * Number of bytes in a q-gram.
+ * Anchor and chain hash functions defined below must be written to process this number of bytes.
+ */
+#define	Q     2
+
+/*
+ * Functions and calculated parameters.
+ * Hash functions must be written to use the number of bytes defined in Q. They scan backwards from the initial position.
+ */
+#define HASH(x, p, s)     ((((x)[(p)]) << (s)) + ((x)[(p) - 1]))    // General hash function using a bitshift for each byte added.
+#define ANCHOR_HASH(x, p) HASH((x), (p), (S1))                      // Hash function for anchor hashes, using the S1 bitshift.
+#define CHAIN_HASH(x, p)  HASH((x), (p), (S3))                      // Hash function for chain hashes, using the S3 bitshift.
+#define FINGERPRINT(H)    (1U << ((H) & 0x1F))                      // Hash fingerprint, taking low 5 bits of the hash to set one of 32 bits.
+#define TABLE_MASK        (ASIZE - 1)                               // Mask for table is one less than the power of two size.
+#define Q2                (Q + Q)                                   // 2 Qs.
+#define END_FIRST_QGRAM   (Q - 1)                                   // Position of the end of the first q-gram.
+#define END_SECOND_QGRAM  (Q2 - 1)                                  // Position of the end of the second q-gram.
+#define CEIL_DIV(n, d)    ((int) ceil((double) (n) / (d)))          // Returns the integer ceiling division of numerator and denominator.
+#define CHAIN_LENGTH      ((CEIL_DIV(log2(ASIZE), (S2)) + 1) * (Q)) // Length required to synchronise with the rolling hash chain.
 
 /*
  * Builds the hash table B of size ASIZE for a string x of length m.
+ * Returns the 32-bit hash value of matching the entire pattern.
  */
-unsigned int preprocessing(unsigned char *x, int m, unsigned int *B) {
-    unsigned int H, H2;
+unsigned int preprocessing(const unsigned char *x, int m, unsigned int *B) {
 
-    const int rollingHashLimit = (LOG2(ASIZE) * Q / S2) + (Q * 2); // TODO: FIX - only need one more qgram - but the loop below takes one away so we add two here...
-    const int fact = m < rollingHashLimit ? m : rollingHashLimit;
-
-    // 0. Zero out the entries in the table.
+    // 0. Zero out the hash table.
     for (int i = 0; i < ASIZE; i++) B[i] = 0;
 
-    // 1. Process all the qgrams that will have a qgram preceding it
-    //    and limiting how long the hash chain is by the rolling hash limit.
-    for (int anchor = (2*Q) - 1; anchor < m - 1; anchor++) {
+    // 1. Process all the anchor q-grams with q-grams before them.
+    unsigned int H;
+    for (int anchor_pos = END_SECOND_QGRAM; anchor_pos < m; anchor_pos++) {
+        H = ANCHOR_HASH(x, anchor_pos);
+        int start_chain = anchor_pos - Q;
+        int stop_chain = MAX(END_FIRST_QGRAM, start_chain - CHAIN_LENGTH);
+        for (int chain_pos = start_chain; chain_pos >= stop_chain; chain_pos -= Q) {
+            unsigned int H_last = H;
+            H = (H << S2) + CHAIN_HASH(x, chain_pos);
+            B[H_last & TABLE_MASK] |= FINGERPRINT(H);
+        }
+    }
+
+    // 2. Process the first q-grams at the start of the pattern that have no preceding q-grams.
+    //    There is no q-gram before them that we can calculate a fingerprint of, to put in their hash table entry.
+    //    However, there is equally no check on its content other than it not being zero.
+    //    If it is currently empty, set it to the fingerprint of the inverse of the current hash value, to avoid pointing back to ourselves.
+    int stop = MIN(m, END_SECOND_QGRAM);
+    for (int anchor = END_FIRST_QGRAM; anchor < stop; anchor++) {
         H = ANCHOR_HASH(x, anchor);
-        int stop = (anchor - fact + 1) > (Q - 1) ? (anchor - fact + 1) :  (Q - 1);
-        for (int chainPos = anchor - Q; chainPos >= stop; chainPos -= Q) {
-            H2 = H;
-            H = (H << S2) + CHAIN_HASH(x, chainPos);
-            B[H2 & TABLE_MASK] |= FINGERPRINT(H);
-        }
+        if (!(B[H & TABLE_MASK])) B[H & TABLE_MASK] = FINGERPRINT(~H);
     }
 
-    // 2. Now continue the hash chain for the last qgram down to q-1 to get the cumulative matching hash value.
-    //TODO: we only need to process final chain for a rolling hash limit of 32 bits - this will be a lot smaller than a big string.
-    H = ANCHOR_HASH(x, m - 1);
-    for (int chainPos = m - 1 - Q; chainPos >= Q - 1; chainPos -= Q) {
-        H2 = H;
-        H = (H << S2) + CHAIN_HASH(x, chainPos);
-        B[H2 & TABLE_MASK] |= FINGERPRINT(H);
+    // 3. Calculate the 32-bit hash value we check when we need to verify a match.
+    //    This is the total 32-bit rolling hash value we would see if processing the entire pattern back to the start.
+    int final_pos = m - 1;
+    H = ANCHOR_HASH(x, final_pos);
+    for (int chain_pos = final_pos - Q; chain_pos >= END_FIRST_QGRAM; chain_pos -=Q)
+        H = (H << S2) + CHAIN_HASH(x, chain_pos);
+
+    return H; // Return 32-bit hash value for processing the entire pattern.
+}
+
+/*
+ * Searches the chain backwards and verifies a match if it scans back to the start of the pattern.
+ */
+inline void search_chain(int *pos, int *count, unsigned int H, unsigned int V, const unsigned int B[ASIZE],
+                         const unsigned char *x, const int m, const unsigned char *y, const unsigned int Hm)
+{
+    const int end_second_qgram_pos = *pos - m + Q2;
+    while (*pos >= end_second_qgram_pos)
+    {
+        *pos -= Q;
+        H = (H << S2) + CHAIN_HASH(y, *pos);
+        if (!(V & FINGERPRINT(H))) return;
+        V = B[H & TABLE_MASK];
     }
 
-    // 3. Process the first Q qgrams.  They do not have a preceding qgram, but still need an entry in the table to
-    // indicate their presence, if one does not already exist.  We don't have to set anything if there's already
-    // something there after all other processing.  Doing this last ensures we only set additional bits if none
-    // are already present for other reasons.
-    int stop = m < (2*Q) ? m - 1 : (2*Q) - 1;
-    for (int anchor = Q-1; anchor <= stop; anchor++) {
-        H2 = ANCHOR_HASH(x, anchor);
-
-        // If there's nothing in this hash table entry already, put in a fingerprint.
-        //TODO: the XOR isn't necessarily helpful - work this out better.
-
-        // reversing the bits of the actual hash of the data value it is, so we don't create a succession loop,
-        // setting its fingerprint to be itself implies it follows itself.  The value itself doesn't really matter,
-        // other than we want it to be randomized to some extent and not to point to itself.
-        if (!(B[H2 & TABLE_MASK])) {
-            B[H2 & TABLE_MASK] = FINGERPRINT(CHAIN_HASH(x, anchor) ^ 0xFF);
-        }
+    *pos = end_second_qgram_pos - Q;
+    if (H == Hm && memcmp(y + *pos - END_FIRST_QGRAM, x, m) == 0) {
+        (*count)++;
     }
-
-    return H;  // Return the hash value of processing the entire pattern (step 2)
 }
 
 /*
@@ -90,15 +146,12 @@ unsigned int preprocessing(unsigned char *x, int m, unsigned int *B) {
  */
 int search(unsigned char *x, int m, unsigned char *y, int n) {
     if (m < Q) return -1;  // have to be at least Q in length to search.
-
-    const int MQ  = m - Q;
-    const int MQQ = MQ - Q;
-    const int MQ1 = MQ + 1;
-    unsigned int H, Hm, V, B[ASIZE];
+    unsigned int H, V, B[ASIZE];
 
     /* Preprocessing */
     BEGIN_PREPROCESSING
-    Hm = preprocessing(x, m, B); // Hm is the total hash of processing the entire pattern back from the end to the start.
+    const int MQ1 = m - Q + 1;
+    const unsigned int Hm = preprocessing(x, m, B);
     END_PREPROCESSING
 
     /* Searching */
@@ -108,47 +161,10 @@ int search(unsigned char *x, int m, unsigned char *y, int n) {
     while (pos < n) {
         H = ANCHOR_HASH(y, pos);
         V = B[H & TABLE_MASK];
-        if (V) { // If the hash entry is not empty, we have a potential match for the anchor hash
-            const int end_second_qgram_pos = pos - MQQ;
-            chain:
-            // As long as we're not before the end of the second qgram, we can move back Q and then read back Q bytes back from there safely.
-            if (pos >= end_second_qgram_pos) {
-                pos -= Q;
-                H = (H << S2) + CHAIN_HASH(y, pos);
-                if (V & FINGERPRINT(H)) {  // if next address fingerprint is in our current value,
-                    V = B[H & TABLE_MASK]; // get the next value.
-                    goto chain;            // and go round again.
-                }
-                // Did not find fingerprint of next hash - there is no match.  Drop through to pos += MQ1 and go round the main loop again.
-            } else { // We read back as far as we can.  Check that the rolling hash equals Hm and if so, verify a match.
-                pos = end_second_qgram_pos - Q;
-                if (H == Hm && memcmp(y + pos - Q + 1, x, m) == 0) {
-                    count++;
-                }
-            }
-
-            /*
-            const int endFirstQgram = pos - MQ;
-            chain:
-            // As long as we're not before the end of the second qgram, we can move back Q and then read back Q bytes back from there safely.
-            if (pos >= endFirstQgram + Q) {
-                pos -= Q;
-                H = (H << S2) + CHAIN_HASH(y, pos);
-                if (V & FINGERPRINT(H)) {  // if next address fingerprint is in our current value,
-                    V = B[H & TABLE_MASK]; // get the next value.
-                    goto chain;            // and go round again.
-                }
-                // Did not find fingerprint of next hash - there is no match.  Drop through to pos += MQ1 and go round the main loop again.
-            } else { // We read back as far as we can.  Check that the rolling hash equals Hm and if so, verify a match.
-                if (H == Hm && memcmp(y + endFirstQgram - Q + 1, x, m) == 0) {
-                    count++;
-                }
-                pos = endFirstQgram;
-            }
-             */
-        }
+        if (V) search_chain(&pos,  &count, H, V, B, x, m, y, Hm);
         pos += MQ1;
     }
     END_SEARCHING
+
     return count;
 }
